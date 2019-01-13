095d4a4e266581d970768e5e50286afa8f991734
Akshay. K <kshykm@amazon.com>
2019-01-13 19:49:31 +0530

    Added service for deleting and updating particular record

--- server/controller/studentController.js
+++ server/controller/studentController.js
@@ -20,12 +20,34 @@ router.post('/addStudent', (req, res) => {
 });
 
 /* Get all Student details */
-router.get('/addStudent/:name', (req, res, next) => {
-    studentDetails.find({'name':req.params.name},(err, data) => {
+router.get('/getStudent/:name', (req, res, next) => {
+    studentDetails.find({ 'name': req.params.name }, (err, data) => {
         if (err) res.send(err);
         res.json(data);
     });
 });
+
+/* Delete one particular student record */
+router.get('/delete/:id', (req, res) => {
+    console.log(req.params.id);
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send('No records: ${req.params.id}');
+    studentDetails.deleteOne({ _id: req.params.id }, (err, data) => {
+        if (!err) res.send(data);
+        else console.log('Error msg' + err);
+    })
+})
+
+/* Find and modify one particular student record */
+router.get('/update/:id', (req, res) => {
+    console.log(req.params.id);
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send('No records: ${req.params.id}');
+    studentDetails.findOneAndUpdate({ _id: req.params.id }, (err, data) => {
+        if (!err) res.send(data);
+        else console.log('Error msg' + err);
+    })
+})
 
 /* Export router */
 module.exports = router;
